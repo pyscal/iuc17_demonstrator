021c0ff2bf1997459475834bf618a054e7079b49
Sarath Menon <sarathmenon@mailbox.org>
2023-06-26 21:50:31 +0200

    add gui elements

---
+++ pyscal_rdf_gui/sparql_query.py
@@ -0,0 +1,25 @@
+from ipywidgets import HBox, VBox, Layout
+from pyscal_rdf_gui.create import output, dropdown, button, checkbox, textbox, header, upload
+
+class SparqlQuery:
+    def __init__(self, theme="teal"):
+        #create widgets
+        self.query_box = textbox("", 
+                                value="""
+            PREFIX cmso: <https://purls.helmholtz-metadaten.de/cmso/>
+            SELECT DISTINCT ?symbol
+            WHERE {
+                ?sample cmso:hasNumberOfAtoms ?number .
+                ?sample cmso:hasMaterial ?material .
+                ?material cmso:hasStructure ?structure .
+                ?structure cmso:hasSpaceGroupSymbol ?symbol .
+            FILTER (?number="2"^^xsd:integer)
+            }
+            """,
+            dtype="textarea")
+        self.output = output(theme=theme)
+        self.run_button = button('Query', 
+                                 theme=theme)
+        self.panel = VBox(children=[self.query_box,
+                                   self.run_button,
+                                   self.output])

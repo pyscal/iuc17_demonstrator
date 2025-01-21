77c330dc96313cf342855b0bc3ac4c0abc7acffb
Sarath <sarathmenon@mailbox.org>
2025-01-21 15:22:12 +0100

    update example

--- pyscal_rdf_gui/image_elements.py
+++
@@ -1,5 +0,0 @@
-import ipywidgets as widgets
-
-image_widget = widgets.HTML("""
-<img src="https://datashare.mpcdf.mpg.de/index.php/apps/files_sharing/ajax/publicpreview.php?x=1920&y=619&a=true&file=logo_pyscalrdf.png&t=WqC6Aqc8zeC2fBp&scalingup=0" width="20%" align="justify">
-""")
